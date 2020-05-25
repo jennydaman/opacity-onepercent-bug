c98943de9f8b90df501f5a2690498d7f21d7ec5c
Jennings Zhang <jenni_zh@protonmail.com>
2020-05-25 01:57:25 -0400

    Picture example

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,6 +5,9 @@ const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
+		<div style="background-color: red">
+			<img className={style.withOpacity} src="/assets/picture.png"/>
+		</div>
 	</div>
 );
 
